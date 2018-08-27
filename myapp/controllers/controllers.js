206561cbc2cded1ba05c84f14d40fb746b6db8b2
shantanu <shantanu.gade@cuelogic>
2018-08-27 14:37:28 +0530

    added ip ua to useractivity schema

--- myapp/controllers/controllers.js
+++ myapp/controllers/controllers.js
@@ -5,6 +5,8 @@ var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var request = require('request');
 var moment = require('moment');
+var useragent = require('express-useragent');
+const requestIp = require('request-ip');
 
 
 require('dotenv').config();
@@ -36,41 +38,48 @@ module.exports = {
                             if (req.body.email === result.email) {
                                 console.log("Authentication Success");
 
+                                var source = req.headers['user-agent'],
+                                    ua = useragent.parse(source);
 
-                                    userActivityModel.findOne({userName:req.body.email},(err,person)=>{
-                                            if(err) {
-                                                console.log("====>err",err) 
-                                            } 
-                                            else if(person===null){
+                                var clientIp = requestIp.getClientIp(req);
 
-                                                var userActivity = new userActivityModel({
-                                                    userId: result._id,
-                                                    userName:result.email,
-                                                    timeStamp: moment().format('x')
-                                                });
-                
-                                                userActivity.save();                                                
-                                            }
-                                            
-                                            else if(person) {
-                                                
-                                                var currentTime = moment().format('x')
-                                                userActivityModel.findOneAndUpdate({userName:result.email},{timeStamp: currentTime});
-                                            }
-                                    })                                
+                                userActivityModel.findOne({ userName: req.body.email }, (err, person) => {
+                                    if (err) {
+                                        console.log("====>err", err)
+                                    }
+                                    else if (person === null) {
+
+                                        var userActivity = new userActivityModel({
+                                            userId: result._id,
+                                            userName: result.email,
+                                            UA:ua.source,
+                                            IP:clientIp,
+                                            timeStamp: moment().format('x')
+                                        });
+
+                                        userActivity.save();
+                                    }
+
+                                    else if (person) {
+
+                                        var currentTime = moment().format('x')
+                                        userActivityModel.findOneAndUpdate({ userName: result.email }, { timeStamp: currentTime });
+                                    }
+                                })
 
 
 
                                 jwt.sign({ userId: result._id }, process.env.SECRETKEY, { expiresIn: '1h' }, (err, token) => {
                                     storeToken(token);
-                                    res.json({ success: true, 
-                                        isAdmin: result.isAdmin, 
+                                    res.json({
+                                        success: true,
+                                        isAdmin: result.isAdmin,
                                         authorization: token,
                                     });
                                 });
                                 request.post("http://localhost:3000/home");
-                               
-                               
+
+
                             }
                         }
                         else {
@@ -93,7 +102,7 @@ module.exports = {
 
         else if (req.method === 'POST') {
             var user = new userModel({
-                 _id:new mongoose.Types.ObjectId(),
+                _id: new mongoose.Types.ObjectId(),
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
                 email: req.body.email,
@@ -139,38 +148,38 @@ module.exports = {
     },
 
     homeAdmin: (req, res, next) => {
-        
+
         if (req.method === 'GET') {
             res.render('homeAdmin');
         }
         else if (req.method === 'POST') {
-      console.log("==>in homeadminpost")
+            console.log("==>in homeadminpost")
 
-            var currentTime =moment().format('x');
+            var currentTime = moment().format('x');
             userActivityModel.find({})
-            .then((users)=>{
-                var userLoggedIn = [];
-                users.forEach(user => {
-                  
-                    if(moment.duration(currentTime-user.timeStamp).asSeconds() > 432000 ){
-                        
-                        userLoggedIn.push(user);
-                    }
+                .then((users) => {
+                    var userLoggedIn = [];
+                    users.forEach(user => {
 
-                    // var updatedUser = {
-                    //     userName : user.userName,
-                    //     timeStamp : moment.duration(currentTime-user.timeStamp).asSeconds()                        
-                    // }
-                    // userLoggedIn.push(updatedUser);
+                        if (moment.duration(currentTime - user.timeStamp).asSeconds() > 432000) {
+
+                            userLoggedIn.push(user);
+                        }
+
+                        // var updatedUser = {
+                        //     userName : user.userName,
+                        //     timeStamp : moment.duration(currentTime-user.timeStamp).asSeconds()                        
+                        // }
+                        // userLoggedIn.push(updatedUser);
 
 
-                });
-                console.log("outside===>",userLoggedIn);
-                  ;res.json({userData:userLoggedIn})  
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
+                    });
+                    console.log("outside===>", userLoggedIn);
+                    ; res.json({ userData: userLoggedIn })
+                }).catch((err) => {
+                    console.log(err);
+                })
+        }
     },
 
     userProfile: (req, res, next) => {
@@ -181,20 +190,20 @@ module.exports = {
 
         }
         else if (req.method === 'POST') {
-           
-            console.log("=====> from authentication" , req.body.userId);
-                    userModel.findOne({ _id: req.body.userId }, (err, result) => {
 
-                        if (result === null) {
+            console.log("=====> from authentication", req.body.userId);
+            userModel.findOne({ _id: req.body.userId }, (err, result) => {
 
-                            console.log("Authentication Failed DATA NOT FOUND");
-                        }
-                        else {
-                            res.json(result);
-                        }
-                    });
+                if (result === null) {
+
+                    console.log("Authentication Failed DATA NOT FOUND");
                 }
-              
+                else {
+                    res.json(result);
+                }
+            });
+        }
+
     },
 
     userProfileAdmin: (req, res, next) => {
@@ -206,17 +215,17 @@ module.exports = {
         }
         else if (req.method === 'POST') {
             // console.log("token=====>",process.env.TOKEN);
-                  userModel.findOne({ _id: req.body.userId }, (err, result) => {
+            userModel.findOne({ _id: req.body.userId }, (err, result) => {
 
-                        if (result === null) {
+                if (result === null) {
 
-                            console.log("Authentication Failed DATA NOT FOUND");
-                        }
-                        else {
-                            res.json(result);
-                        }
-                    });
-                
+                    console.log("Authentication Failed DATA NOT FOUND");
+                }
+                else {
+                    res.json(result);
+                }
+            });
+
         }
     },
 
@@ -262,61 +271,63 @@ module.exports = {
     },
 
 
-    updateUserinDb : (req,res,next) => {
+    updateUserinDb: (req, res, next) => {
         if (req.method === 'GET') {
             res.render('updateUser');
         }
         else if (req.method === 'POST') {
-       
-            userModel.findOneAndUpdate({_id:req.body.userId}, { firstName:req.body.userFirstName,
-            lastName: req.body.userLastName }, (err,doc)=>{
-                if(err) {
-                   console.log(err)
-                    res.json({success:false});
+
+            userModel.findOneAndUpdate({ _id: req.body.userId }, {
+                firstName: req.body.userFirstName,
+                lastName: req.body.userLastName
+            }, (err, doc) => {
+                if (err) {
+                    console.log(err)
+                    res.json({ success: false });
                 }
                 else {
                     console.log(doc);
-                    res.json({success:true})
+                    res.json({ success: true })
                 }
             });
 
         }
     },
 
-    allUser : (req,res,next) => {
+    allUser: (req, res, next) => {
         if (req.method === 'GET') {
             res.render('allUser');
         }
         else if (req.method === 'POST') {
-           userModel.find({},(err,usersData) => {
-               if(err) {
-                   console.log(err)
-                   res.json({success:false})
-               }
-               else {
-                   
-                   res.json({success:true,users:usersData});
-               }
-           })
+            userModel.find({}, (err, usersData) => {
+                if (err) {
+                    console.log(err)
+                    res.json({ success: false })
+                }
+                else {
+
+                    res.json({ success: true, users: usersData });
+                }
+            })
         }
     },
     searchUser: (req, res, next) => {
         if (req.method === 'GET') {
             res.render('searchUser');
-           
+
         }
         else if (req.method === 'POST') {
-            userModel.findOne({email :req.body.email }, (err, result) => {
+            userModel.findOne({ email: req.body.email }, (err, result) => {
 
                 if (result === null) {
                     //res.boom.unauthorized("User Not present");
                     res.json(result);
-                    
+
                 }
                 else {
                     res.json(result);
                 }
             });
         }
-    }, 
+    },
 }
